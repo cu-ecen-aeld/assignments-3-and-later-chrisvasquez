2d2c8ef5177105ceee831638591f2a3fa2833394
chrisvasquez <chrisvasquez@AELD.myguest.virtualbox.org>
2023-07-03 10:59:29 +1000

    Assignment 4 Part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -11,9 +12,26 @@
 void* threadfunc(void* thread_param)
 {
 
+
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = ((struct thread_data *)thread_param)->wait_to_obtain_ms * 1000;
+    nanosleep(&ts, &ts);
+
+    pthread_mutex_lock(((struct thread_data *)thread_param)->mutex);
+
+    ts.tv_sec = 0;
+    ts.tv_nsec = ((struct thread_data *)thread_param)->wait_to_release_ms * 1000;
+    nanosleep(&ts, &ts);
+
+    pthread_mutex_unlock(((struct thread_data *)thread_param)->mutex);
+
+    ((struct thread_data *)thread_param)->thread_complete_success = true;
+    
     return thread_param;
 }
 
@@ -28,6 +46,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *tData = (struct thread_data *)malloc(sizeof(struct thread_data));
+    
+    tData->mutex = mutex;
+    tData->wait_to_obtain_ms = wait_to_obtain_ms;
+    tData->wait_to_release_ms = wait_to_release_ms;
+
+    // struct timespec ts;
+    // ts.tv_sec = 0;
+    // ts.tv_nsec = wait_to_obtain_ms * 1000;
+    // nanosleep(&ts, &ts);
+
+    // pthread_mutex_lock(mutex);
+
+    // ts.tv_sec = 0;
+    // ts.tv_nsec = wait_to_release_ms * 1000;
+    // nanosleep(&ts, &ts);
+
+    // pthread_mutex_unlock(mutex);
+
+    int rc = pthread_create(thread, NULL, threadfunc, tData);
+
+    return rc == -1 ? false : true;
 }
 
